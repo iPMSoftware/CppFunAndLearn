1a1d1f9e229ec4dc387efb31dc170aa1730c0102
Throghar <pawel.mirgos@gmail.com>
2022-02-10 21:29:46 +0100

    * Changed name to reflect functionality of backward iteration * Added possibility to use other functions for every element
    Signed-off-by: Throghar <pawel.mirgos@gmail.com>

--- template_recursive_iteration_over_tuple/main.cpp
+++ template_recursive_iteration_over_tuple/main.cpp
@@ -17,20 +17,31 @@ struct is_non_negative : true_type {};
 template<>
 struct is_non_negative<-1> : false_type {};
 
-template<int index, typename... T>
-typename std::enable_if<!is_non_negative<index>::value,int>::type
-Iterate(std::tuple<T...>& t) {
-    return 0;
+template<int index, typename F, typename... T>
+typename std::enable_if<!is_non_negative<index>::value>::type
+IterateBackward(F f,std::tuple<T...>& t) {}
+
+template<int index, typename F,typename... T>
+typename std::enable_if<is_non_negative<index>::value>::type
+IterateBackward(F f,std::tuple<T...>& t) {
+    f(std::get<index>(t));
+    IterateBackward<index-1>(f,t);
 }
 
-template<int index, typename... T>
-typename std::enable_if<is_non_negative<index>::value,int>::type
-Iterate(std::tuple<T...>& t) {
-    return std::get<index>(t) + Iterate<index-1>(t);
+template<typename T>
+void print(T x) {
+    std::cout << x << std::endl;
+}
+
+template<typename T>
+void multimply(T x) {
+    std::cout << (x*2) << std::endl;
 }
 
 int main() {
     std::tuple<int,int,int> t{1,2,3};
-    auto result = Iterate<2>(t);
-    std::cout << "result " << result << std::endl;
+    constexpr auto max_tuple_index = std::tuple_size<decltype(t)>{}-1;
+    using SelectedType = std::tuple_element_t<max_tuple_index, decltype(t)>;
+    IterateBackward<max_tuple_index>(print<SelectedType>,t);
+    IterateBackward<max_tuple_index>(multimply<SelectedType>,t);
 }
